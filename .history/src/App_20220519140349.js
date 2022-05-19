8f2fdb361ffe9ee500a29cdc2d66711fca9a9a6f
Odell Purifoy <opurifoy@springhealth.com>
2022-05-19 14:24:19 -0400

    Didnt add jumbotron just fixed some tabbing issues

---
+++ .history/src/App_20220519140349.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  ApolloClient,
+  // createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { gql } from 'apollo-boost';
+import { Container, Navbar } from 'react-bootstrap';
+import AllUsers from './components/AllUsers';
+import AllPosts from './components/AllPosts';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navigation from './components/layout/Navigation';
+import JumbotronComponent from './components/layout/JumbotronComponent';
+
+const link = from([
+  new HttpLink({ uri: 'http://localhost:3003/graphql' })
+])
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Navigation />
+      <JumbotronComponent />
+      <Container>
+        <AllPosts />
+        <AllUsers />
+      </Container>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ .history/src/components/layout/JumbotronComponent_20220519140200.js
@@ -0,0 +1,27 @@
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
+
+import React from 'react'
+
+const JumbotronComponent = () => {
+  return (
+      <>
+        <Jumbotron fluid>
+            <Container>
+                <h1>Fluid jumbotron !</h1>
+                <p>
+                This is a modified fluid jumbotron which
+                stretches the whole horizontal space.    
+                </p>
+                <Button variant="primary">
+                Primary Button
+                </Button>
+            </Container>
+        </Jumbotron>
+      </>
+  )
+}
+
+export default JumbotronComponent;
